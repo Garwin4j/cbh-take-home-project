edfc213bce5fba329338e0b67d807f15890f95b6
Garwin Pryce <garwin.pryce@gmail.com>
2023-02-27 08:35:23 -0500

    Refactor function

--- Refactoring.md
+++ Refactoring.md
@@ -9,3 +9,5 @@ You've been asked to refactor the function `deterministicPartitionKey` in [`dpk.
 You will be graded on the exhaustiveness and quality of your unit tests, the depth of your refactor, and the level of insight into your thought process provided by the written explanation.
 
 ## Your Explanation Here
+
+There where some redundant checks that I thing was simplified by adding a guard clause on line 8. My general aim was to reduce complexity by reducing the number of if else clauses the reader would have to follow.  
--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,24 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns a key when a string is given as input", () => {
+    const trivialKey = deterministicPartitionKey();
+    expect(trivialKey).toBeTruthy();
+  });
+
+  it("Returns the same key for `{data: 1, partitionKey: `abc`} and `abc` as inputs", () => {
+    const trivialKeyOne = deterministicPartitionKey({data: 1, partitionKey: 'abc'});
+    expect(trivialKeyOne).toBeTruthy();
+    const trivialKeyTwo = deterministicPartitionKey({data: 1, partitionKey: 'abc'});
+    expect(trivialKeyTwo).toBeTruthy();
+    expect(trivialKeyOne).toBe(trivialKeyTwo)
+  });
+
+  it("Returns a key for `{data: 1, partitionKey: {name: `test`, id: 34}}` as inputs", () => {
+    const trivialKeyOne = deterministicPartitionKey({data: 1, partitionKey: {name: `test`, id: 34}});
+    expect(trivialKeyOne).toBeTruthy();
+   
+  });
+
 });
